f7afd3d135d64a5c1101ba43980d31b9df5044f7
chaop <joeypan123@hotmail.com>
2012-09-27 06:25:44 +0000

    add roll over function to front end page

--- public/lightbox/lightbox_display.php
+++ public/lightbox/lightbox_display.php
@@ -14,11 +14,27 @@ function lightbox_display(){;
               $result = mysql_query($select_sql) or Flash::setNow('error', __('Cannot select from database'));
               $image = mysql_result($result, 0, 'image_name');
               $thumbnail = mysql_result($result, 0, 'thumbnail_name');
-              $order = mysql_result($result, 0, 'order_number');
+              $rollover = mysql_result($result, 0, 'rollover_name');
+	      $order = mysql_result($result, 0, 'order_number');
               $image_path = URL_PUBLIC . 'public/gallery/' . $image;
               $thumbnail_path = URL_PUBLIC . 'public/gallery/thumbnails/' . $thumbnail;
-            
-              echo "<a href='$image_path' rel='lightbox[image]' title='$image'><img src='$thumbnail_path' />";
+              $rollover_path = URL_PUBLIC . 'public/gallery/rollovers/' . $rollover;
+
+	      echo "<script src='js/jquery-1.7.2.min.js'></script>";		
+              echo "<a class='image$curr' href='$image_path' rel='lightbox[image]' title='$image'><img src='$thumbnail_path' />";
+	      if($rollover != '')
+	      {	
+	      	echo "<script>
+			$('.image$curr').hover(
+				function() {
+					$('.image$curr img').attr('src', '$rollover_path');
+				},
+				function() {
+					$('.image$curr img').attr('src', '$thumbnail_path');
+				}
+			);
+		    </script>";
+	      }
             }
           }
           mysql_close($db);
